86578006f9f4e374cc2ec0bbfcbc7f06cd9e968d
Shirsak Majumder <shirsakm@gmail.com>
2024-10-31 14:42:00 +0530

    Added loading functionality

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from pn import Scraper
 scraper = Scraper('https://publicnotices.washingtonpost.com/')
 
 scraper.load_url()
+scraper.execute_search('Palace')
+scraper.load_all_notices()
 scraper.get_notices()
 
 scraper.close()
--- pn/scraper.py
+++ pn/scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pprint import pprint
 
 class Scraper:
@@ -11,20 +12,49 @@ class Scraper:
 
     def load_url(self):
         self.driver.get(self.URL)
-        sleep(20)
+        WebDriverWait(self.driver, 30) \
+            .until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[2]/div[5]/div[3]/button')))
+
+
+    def load_all_notices(self):
+        WebDriverWait(self.driver, 30) \
+            .until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[2]/div[5]/div[1]')))
+        
+        notice_number = int(self.driver \
+            .find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[2]/div[5]/div[1]') \
+            .get_attribute('innerHTML') \
+            .split(' ')[1])
+
+        if notice_number < 80:
+            return
+
+        iters_required = int(notice_number) // 80 + 1
+        for _ in range(iters_required):
+            load_more = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[2]/div[5]/div[3]/button')
+            load_more.click()
+
+            WebDriverWait(self.driver, 30) \
+                .until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[2]/div[5]/div[3]/button')))
+
 
     def execute_search(self, search=None):
-        pass
+        if not search:
+            return
+
+        search_input = self.driver.find_element(By.ID, 'searchbtn')
+        search_input.send_keys(search)
+        search_button = self.driver.find_element(By.ID, 'search')
+        search_button.click()
+        
+        WebDriverWait(self.driver, 30) \
+            .until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[2]/div[1]')))
 
 
     def get_notices(self):
-        load_more = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[2]/div[5]/div[3]/button')
-        load_more.click()
-        sleep(20)
         notices = self.driver.find_elements(By.CLASS_NAME, 'public-notice-result')
-        id_list = []
+        id_list = set()
         for notice in notices:
-            id_list.append(notice.get_attribute('data-notice-id'))
+            id_list.add(notice.get_attribute('data-notice-id'))
         pprint(id_list)
         print(len(id_list))
 
