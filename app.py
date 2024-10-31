e7a6b5004ea1f97fd91a7298cb59604cc3e5707d
Shirsak Majumder <shirsakm@gmail.com>
2024-11-01 00:03:11 +0530

    Added date time query

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from pn import Scraper
+from datetime import datetime
 
 scraper = Scraper('https://publicnotices.washingtonpost.com/')
 
 scraper.load_url()
-scraper.execute_search('Palace')
-scraper.load_all_notices()
-scraper.get_notices()
+# scraper.execute_search(start_date=datetime(2024, 10, 20))
+# scraper.load_all_notices()
+# scraper.get_notices()
 
 scraper.close()
