7a055add3a60f9871ff1510c8be83270bc4d5559
Shirsak Majumder <shirsakm@gmail.com>
2024-11-01 17:56:07 +0530

    Added sample search queries

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from wppn import Scraper
 from datetime import datetime
 
 scraper = Scraper('https://publicnotices.washingtonpost.com/')
-scraper.search(start_date=datetime(2024, 10, 15), states=['DC'])
+
+"""
+Find below, sample search queries, uncomment to run
+"""
+scraper.search(search_phrase='Palace')
+# scraper.search(start_date=datetime(2024, 10, 15), states=['DC'])
+# scraper.search(counties=['Prince George\'s'], notice_types=['Trustee Sales'])
+
 scraper.save_all_notices('./data/notices')
 scraper.close()
